283a2cf30ddeaa387496993edbf8c06d389110c8
ChaoticQubit <armansinghal@ufl.edu>
2023-09-14 09:06:26 -0400

    Adding Spiral

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 let data;
 let months;
+let monthsLength;
 
 let zeroRadius = 100;
 let oneRadius = 200;
 let monthsRadius = 300;
+
+let currentRow = 0;
+let currentMonth = 0;
 
 function preload() {
   data = loadTable('Data/dSST.csv', 'csv', 'header');
@@ -12,18 +16,19 @@ function preload() {
 function setup() {
   createCanvas(windowHeight, windowWidth);
   months = data.columns.slice(1, 13);
+  monthsLength = months.length;
 }
 
 function draw() {
   background(0);
   translate(width/2, height/2);
+  textAlign(CENTER, CENTER);
+  textSize(24);
   createCircle(zeroRadius * 2, content="0°");
   createCircle(oneRadius * 2, content="1°");
   createCircle(monthsRadius * 2);
-  let row = data.getRow(0);
-  let year = row.get("Year");
-  textAlign(CENTER, CENTER);
-  text(year, 0, 0)
+  getYear();
+  createSpiral();
 }
 
 function createCircle(size, content=""){
@@ -41,15 +46,11 @@ function cirleText(size, deg=""){
     noStroke();
     text(deg, (size / 2) + 20, 0)
   }else{
-    for (let i = 0; i < months.length; i++){
+    for (let i = 0; i < monthsLength; i++){
       noStroke();
       fill(255);
-      textAlign(CENTER);
-      textSize(24);
-      let angle = map(i, 0, months.length, 0, TWO_PI) - PI/3;
       push();
-      let x = (monthsRadius + 15) * cos(angle);
-      let y = (monthsRadius + 15) * sin(angle);
+      let { x, y, angle } = giveCartesianCoordinates(i, (monthsRadius + 15));
       translate(x, y);
       rotate(angle + PI/2);
       text(months[i], 0, 0);
@@ -58,6 +59,55 @@ function cirleText(size, deg=""){
   }
 }
 
+function getYear(){
+  let year = data.getRow(currentRow).get('Year');
+  text(year, 0, 0);
+}
+
 function createSpiral(){
-  
-} 
+  beginShape();
+  noFill();
+  stroke(255);
+  creatingYearlyDataPoints();
+  endShape();
+}
+
+function creatingYearlyDataPoints(){
+  for(let i = 0; i < currentRow; i++){
+    let row = data.getRow(i);
+
+    let totalMonths = months.length;
+    if(i == currentRow - 1){
+      totalMonths = currentMonth;
+    }
+
+    creatingMonthlyDataPoints(totalMonths, row);
+  }
+
+  currentMonth = currentMonth + 1;
+  if(currentMonth == monthsLength){
+    currentMonth = 0;
+    currentRow++;
+    if (currentRow == data.getRowCount()){
+      noLoop();
+    }
+  }
+}
+
+function creatingMonthlyDataPoints(totalMonths, row){
+  for(let i = 0; i < totalMonths; i++){
+    let anomaly = row.get(months[i]);
+    if(anomaly !== '***'){
+      let r = map(anomaly, 0, 1, zeroRadius, oneRadius);
+      let { x, y } = giveCartesianCoordinates(i, r);
+      vertex(x, y);
+    }
+  }
+}
+
+function giveCartesianCoordinates(value, radius){
+  let angle = map(value, 0, monthsLength, 0, TWO_PI) - PI/3;
+  let x = radius * cos(angle);
+  let y = radius * sin(angle);
+  return { x, y, angle };
+}
