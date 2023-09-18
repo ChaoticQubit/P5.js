aae6ce80dc29c1fb9a6b55a47026e4ab25f932a1
ChaoticQubit <armansinghal@ufl.edu>
2023-09-18 09:30:05 -0400

    update canvas

--- Global-Temperature-Spiral-Animation/src/sketch.js
+++ Global-Temperature-Spiral-Animation/src/sketch.js
@@ -25,7 +25,7 @@ function setup() {
 
 function draw() {
   background(0);
-  translate(width/2, height/2);
+  translate(width, height);
   textAlign(CENTER, CENTER);
   textSize(20);
   createSpiral();
